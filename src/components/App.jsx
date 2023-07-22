68c625afcde0d3b910606a862c61549fbfab4e36
Pratham <prathamkl17@gmail.com>
2023-07-22 10:43:01 +0530

    First Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+
+    const fetchAPI = () => {
+        const apiGet = () => {
+            fetch()
+        }
+    }
+
+    return(
+        <div>Train.</div>
+    )
+}
+
+export default App
